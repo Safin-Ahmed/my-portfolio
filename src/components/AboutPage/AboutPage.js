2ed1458535a0c168026e266f694c4e892138fd76
Safin-Ahmed <safin.ahmed2000@gmail.com>
2021-12-05 21:47:46 +0600

    Added some security

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import About from '../About/About';
 
 const AboutPage = () => {
     return (
         <div>
-            This is About Page
+            <About></About>
         </div>
     );
 };
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -48,10 +48,10 @@ const Contact = () => {
                         freelancing if the right project comes along. me.
                     </p>
                     <form ref={formRef} onSubmit={handleSubmit}>
-                        <input style={{ backgroundColor: darkMode && "#333" }} type="text" placeholder="Name" name="user_name" />
+                        <input required style={{ backgroundColor: darkMode && "#333" }} type="text" placeholder="Name" name="user_name" />
                         <input style={{ backgroundColor: darkMode && "#333" }} type="text" placeholder="Subject" name="user_subject" />
-                        <input style={{ backgroundColor: darkMode && "#333" }} type="email" placeholder="Email" name="user_email" />
-                        <textarea style={{ backgroundColor: darkMode && "#333" }} rows="5" placeholder="Message" name="message"></textarea>
+                        <input required style={{ backgroundColor: darkMode && "#333" }} type="email" placeholder="Email" name="user_email" />
+                        <textarea required style={{ backgroundColor: darkMode && "#333" }} rows="5" placeholder="Message" name="message"></textarea>
                         <button className="contact-btn">Submit</button>
                         {done && <p>Thank You !</p>}
                     </form>
